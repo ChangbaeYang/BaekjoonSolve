e4963d76b300660d073b4b0e28499248690c8139
Zzangbae <109326312+ChangbaeYang@users.noreply.github.com>
2022-11-12 09:56:06 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+arr = [i for i in range(1, 31)]
+for i in range(28):
+    a = int(input())
+    if a in arr:
+        arr.remove(a)
+for j in range(2):
+    print(arr[j])
